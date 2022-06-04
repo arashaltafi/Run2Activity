a851a7efa591ae3496ab2216dab99bbec6e26dd6
Arash Altafi <arashaltafi1377@gmail.com>
2022-06-04 12:24:46 +0430

    Initial commit

---
+++ app/src/main/java/com/arash/altafi/run2activity/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.arash.altafi.run2activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
